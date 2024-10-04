7848270e39bb53cc8703bcee323d3410df8ca8df
Sima Bilony <www.simabilony@gmail.com>
2024-10-04 08:18:30 +0300

    SetUp

---
+++ app/Models/Booking.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'start',
+        'end',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
