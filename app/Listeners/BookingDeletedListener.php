4afd599a4a5f81c674173ef3d43cc400d2284223
Sima Bilony <www.simabilony@gmail.com>
2024-10-12 12:49:50 +0300

    BookingDeletedListener

--- app/Listeners/BookingDeletedListener.php
+++ app/Listeners/BookingDeletedListener.php
@@ -6,21 +6,17 @@ use App\Events\BookingDeletedEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+
 class BookingDeletedListener
 {
-    /**
-     * Create the event listener.
-     */
     public function __construct()
     {
-        //
     }
 
-    /**
-     * Handle the event.
-     */
     public function handle(BookingDeletedEvent $event): void
     {
-        //
+        $event->booking->scheduledNotifications()
+            ->where('user_id', $event->booking->user_id)
+            ->delete();
     }
 }
